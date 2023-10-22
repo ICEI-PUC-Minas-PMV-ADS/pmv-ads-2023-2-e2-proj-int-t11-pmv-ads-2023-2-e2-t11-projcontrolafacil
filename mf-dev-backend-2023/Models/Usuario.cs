7f522a76b9edd936dcda37dd2c4b40eed10ce14e
Geovanne do Monte <geovannedomonte@gmail.com>
2023-10-22 11:29:48 -0300

    Model-Usuario
    Adicionando a classe Usuário
    
    Co-Authored-By: Lucas Soares <77800106+lucsoares03@users.noreply.github.com>

---
+++ mf-dev-backend-2023/Models/Usuario.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace mf_dev_backend_2023.Models
+{
+    [Table("Usuarios")]
+    public class Usuario
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar o nome.")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar a senha.")]
+        [DataType(DataType.Password)]
+        public string Senha { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar o perfil.")]
+        public Perfil Perfil { get; set; }
+    }
+
+    public enum Perfil
+    {
+        Admin,
+        User
+    }
+}
