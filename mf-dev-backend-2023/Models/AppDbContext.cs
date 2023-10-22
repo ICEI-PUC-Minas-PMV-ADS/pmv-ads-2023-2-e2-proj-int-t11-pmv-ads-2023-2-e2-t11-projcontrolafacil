a969b961f133f268dc1c2dc151085e3ec3ccc6fc
Geovanne do Monte <geovannedomonte@gmail.com>
2023-10-22 11:34:46 -0300

    Atualizando Model-AppDbContext
    Atualizando  Dbset do AppDbContext

--- mf-dev-backend-2023/Models/AppDbContext.cs
+++ mf-dev-backend-2023/Models/AppDbContext.cs
@@ -6,8 +6,8 @@ namespace mf_dev_backend_2023.Models
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
-        public DbSet<Veiculo> Veiculos { get; set; }
+        public DbSet<Transacao> Transacoes { get; set; }
 
-        public DbSet<Consumo> Consumos { get; set; }
+        public DbSet<Usuario> Usuarios { get; set; }
     }
 }
